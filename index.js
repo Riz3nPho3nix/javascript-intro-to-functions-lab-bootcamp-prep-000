85032eddd94c08add852329b3e69e5b7b10c281b
Brian Wilson <wahooey87@gmail.com>
2018-02-15 22:24:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function shout (string) {
   return string.toUpperCase()
 }
-function 
+function whisper (string) {
+  return string.toLowerCase()
+}
+function logShout (string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper (string) {
+  console.log(string.toLowerCase())
+}
